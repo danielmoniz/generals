54c542ab55fe630d7e4f06e75c386ac0b3bda7c9
Daniel Moniz <daniel.moniz@gmail.com>
2014-09-13 22:23:32 -0400

    Remove unnecessary code from victory checking.

--- src/victory.js
+++ src/victory.js
@@ -24,21 +24,6 @@ Victory = {
       }
     }
     return false;
-
-    var units = Crafty('Unit').get();
-    var first_player_units = [];
-    var second_player_units = [];
-    for (var i=0; i<units.length; i++) {
-      if (units[i].side == 0) {
-        first_player_units.push(units[i]);
-      } else {
-        second_player_units.push(units[i]);
-      }
-    }
-    if (first_player_units.length == 0 || second_player_units.length == 0) {
-      return true;
-    }
-    return false;
   },
 
   updateWillToFight: function() {
