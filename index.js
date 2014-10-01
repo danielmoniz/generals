9c63cab1cdba9cce33e74fe9ec05c3af5fa0dbdb
Daniel Moniz <daniel.moniz@gmail.com>
2014-10-01 17:23:40 -0400

    Get chat working.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-//var finalhandler = require('finalhandler');
+var finalhandler = require('finalhandler');
 var http = require('http');
 var express = require('express');
 var app = express();
 var serve_static = require('serve-static');
+
+var server = http.createServer(app);
 
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(express.static('public/'));
 
-var io = require('socket.io')(app);
+var io = require('socket.io').listen(server);
 require('./chat/chat-listeners.js')(io);
 require('./chat/game-listeners')(io);
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
