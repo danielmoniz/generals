5ff36ee1dc27830cc3ae01456e48855788dcc83f
Daniel Moniz <daniel.moniz@gmail.com>
2014-10-06 18:27:17 -0400

    Add framework/tests for retreat constraints.

---
+++ public/src/retreat_constraints.js
@@ -0,0 +1,118 @@
+
+var RetreatConstraints = function(location) {
+  if (location === undefined) {
+    throw new Error("BadLocation", "Must specify a location.");
+  }
+  this.location = location;
+  this.area = this.setAdjacentSpaces(location);
+};
+
+RetreatConstraints.prototype.setAdjacentSpaces = function() {
+  var area = [[], [], []];
+  var centre = { x: 1, y: 1 };
+  for (var x in area) {
+    area[x] = [[], [], []];
+    for (var y in area[x]) {
+      area[x][y] = 1;
+      if (x == centre.x) area[x][y] = 0;
+      if (y == centre.y) area[x][y] = 0;
+      if (x == centre.x && y == centre.y) area[x][y] = 1;
+    }
+  }
+
+  return area;
+};
+
+RetreatConstraints.prototype.getArrayDirection = function(direction) {
+  if (!direction || direction.x === undefined || direction.y === undefined) {
+    throw new Error('BadDirection', "Direction is not valid.");
+  }
+  var x = direction.x - this.location.x + 1;
+  var y = direction.y - this.location.y + 1;
+  var distance = Math.sqrt(Math.pow(x - 1, 2) + Math.pow(y - 1, 2));
+  if (distance > 1) throw new Error('BadDirection', "Tile is not adjacent.");
+  return { x: x, y: y };
+};
+
+RetreatConstraints.prototype.setSide = function(side, move_direction) {
+  this.side = side;
+  if (side.toLowerCase() == 'attacker') {
+    return this.setAttacker(move_direction);
+  } else if (side.toLowerCase() == 'defender') {
+    return this.setDefender(move_direction);
+  }
+};
+
+RetreatConstraints.prototype.setAttacker = function(attacker_direction) {
+  var attacker_direction = this.getArrayDirection(attacker_direction);
+  for (var x in this.area) {
+    for (var y in this.area[x]) {
+      if (x == attacker_direction.x && y == attacker_direction.y) {
+        this.area[x][y] = 1;
+      } else {
+        this.area[x][y] = 0;
+      }
+    }
+  }
+
+  this.resetCornersAndCentre();
+  return this.area;
+};
+
+RetreatConstraints.prototype.setDefender = function(attacker_direction) {
+  var attacker_direction = this.getArrayDirection(attacker_direction);
+  for (var x in this.area) {
+    for (var y in this.area[x]) {
+      if (x == attacker_direction.x && y == attacker_direction.y) {
+        this.area[x][y] = 0;
+      } else {
+        this.area[x][y] = 1;
+      }
+    }
+  }
+
+  this.resetCornersAndCentre();
+  return this.area;
+};
+
+RetreatConstraints.prototype.addUnit = function(side, move_direction) {
+  var move_direction = this.getArrayDirection(move_direction);
+  var value = 0;
+  if (this.side == side) {
+    value = 1;
+  }
+  this.area[move_direction.x][move_direction.y] = value;
+};
+
+RetreatConstraints.prototype.applyToArray = function(array, property) {
+  for (var x=0; x<this.area.length; x++) {
+    var real_x = x +  parseInt(this.location.x) - 1;
+    if (array[real_x] === undefined) continue;
+
+    for (var y=0; y<this.area[x].length; y++) {
+      var real_y = y + parseInt(this.location.y) - 1;
+      if (array[real_x][real_y] === undefined) continue;
+      if (property) {
+        array[real_x][real_y][property] *= this.area[x][y];
+      } else {
+        array[real_x][real_y] *= this.area[x][y];
+      }
+    }
+  }
+};
+
+RetreatConstraints.prototype.resetCornersAndCentre = function() {
+  this.area[0][0] = 1;
+  this.area[0][2] = 1;
+  this.area[2][0] = 1;
+  this.area[2][2] = 1;
+
+  this.area[1][1] = 1;
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = RetreatConstraints;
+} else {
+  window.RetreatConstraints = RetreatConstraints;
+}
+
---
+++ test/retreat_constraints.js
@@ -0,0 +1,473 @@
+
+var assert = require("assert");
+
+var RetreatConstraints = require("../public/src/retreat_constraints");
+
+describe('RetreatConstraints', function() {
+
+  var relative_top = { x: 1, y: 0 };
+  var relative_right = { x: 2, y: 1 };
+  var relative_bottom = { x: 1, y: 2 };
+  var relative_left = { x: 0, y: 1 };
+
+  var top = { x: 10, y: 4 };
+  var right = { x: 11, y: 5 };
+  var bottom = { x: 10, y: 6 };
+  var left = { x: 9, y: 5 };
+
+  var point = { x: 10, y: 5 };
+
+  describe('#setAdjacentSpaces()', function() {
+
+    it('should initialize all adjacent spaces to 0 and corners to 1', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      var area = retreat_constraints.setAdjacentSpaces({ x: 0, y: 0 });
+      assert.equal(area[0][1], 0);
+      assert.equal(area[1][0], 0);
+      assert.equal(area[1][2], 0);
+      assert.equal(area[2][1], 0);
+
+      testAreaCornersAndCentre(area);
+    });
+
+  });
+
+  describe('#setAttacker()', function() {
+
+    it('should set attacker constraints s.t they can only retreat to where they came', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setAttacker(top);
+      var area = retreat_constraints.area;
+
+      testTop(area, 1);
+      testRight(area, 0);
+      testBottom(area, 0);
+      testLeft(area, 0);
+
+      testAreaCornersAndCentre(area);
+    });
+
+  });
+
+  describe('#setDefender()', function() {
+
+    it('should set defender constraints s.t they can retreat everywhere except attacking direction', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setDefender(top);
+      var area = retreat_constraints.area;
+
+      testTop(area, 0);
+      testRight(area, 1);
+      testBottom(area, 1);
+      testLeft(area, 1);
+
+      testAreaCornersAndCentre(area);
+    });
+
+  });
+
+  describe('#setSide()', function() {
+
+    it('should correctly set the area array given attacker', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('attacker', left);
+      var area = retreat_constraints.area;
+      
+      assert.equal(retreat_constraints.side, 'attacker');
+      testLeft(area, 1);
+      testTop(area, 0);
+      testBottom(area, 0);
+      testRight(area, 0);
+    });
+
+    it('should correctly set the area array given defender', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('defender', left);
+      var area = retreat_constraints.area;
+      
+      assert.equal(retreat_constraints.side, 'defender');
+      testLeft(area, 0);
+      testTop(area, 1);
+      testBottom(area, 1);
+      testRight(area, 1);
+    });
+
+  });
+
+  describe('#addUnit()', function() {
+
+    it('should disallow attacker retreats when new defenders join', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('attacker', bottom);
+      var area = retreat_constraints.area;
+
+      testBottom(area, 1);
+      retreat_constraints.addUnit('defender', bottom);
+      testBottom(area, 0);
+
+      testLeft(area, 0);
+      testTop(area, 0);
+      testRight(area, 0);
+
+    });
+
+    it('should allow attacker retreats when new attackers join', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('attacker', right);
+      var area = retreat_constraints.area;
+
+      testRight(area, 1);
+
+      testLeft(area, 0);
+      retreat_constraints.addUnit('attacker', left);
+      testLeft(area, 1);
+
+      testBottom(area, 0);
+      retreat_constraints.addUnit('attacker', bottom);
+      testBottom(area, 1);
+
+      testTop(area, 0);
+      retreat_constraints.addUnit('attacker', top);
+      testTop(area, 1);
+    });
+
+    it('should update attacker retreats when new units join', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('attacker', right);
+      var area = retreat_constraints.area;
+
+      testRight(area, 1);
+
+      testLeft(area, 0);
+      retreat_constraints.addUnit('defender', left);
+      testLeft(area, 0);
+
+      testRight(area, 1);
+      retreat_constraints.addUnit('attacker', right);
+      testRight(area, 1);
+
+      testRight(area, 1);
+      retreat_constraints.addUnit('defender', right);
+      testRight(area, 0);
+
+      testRight(area, 0);
+      retreat_constraints.addUnit('attacker', right);
+      testRight(area, 1);
+    });
+
+    it('should disallow defender retreats when new attackers join', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('defender', top);
+      var area = retreat_constraints.area;
+
+      testTop(area, 0);
+      retreat_constraints.addUnit('attacker', top);
+      testTop(area, 0);
+
+      testLeft(area, 1);
+      testBottom(area, 1);
+      testRight(area, 1);
+
+      testRight(area, 1);
+      retreat_constraints.addUnit('attacker', right);
+      testRight(area, 0);
+
+      testBottom(area, 1);
+      retreat_constraints.addUnit('attacker', bottom);
+      testBottom(area, 0);
+
+      testLeft(area, 1);
+      retreat_constraints.addUnit('attacker', left);
+      testLeft(area, 0);
+
+    });
+
+    it('should allow defender retreats when new defenders join', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('defender', right);
+      var area = retreat_constraints.area;
+
+      testRight(area, 0);
+      testLeft(area, 1);
+      testBottom(area, 1);
+      testTop(area, 1);
+
+      testRight(area, 0);
+      retreat_constraints.addUnit('defender', right);
+      testRight(area, 1);
+    });
+
+    it('should update defender retreat constraints when new units join', function() {
+      var retreat_constraints = new RetreatConstraints(point);
+      retreat_constraints.setSide('defender', right);
+      var area = retreat_constraints.area;
+
+      testRight(area, 0);
+      retreat_constraints.addUnit('defender', right);
+      testRight(area, 1);
+
+      testRight(area, 1);
+      retreat_constraints.addUnit('attacker', right);
+      testRight(area, 0);
+
+      testRight(area, 0);
+      retreat_constraints.addUnit('defender', right);
+      testRight(area, 1);
+
+      testLeft(area, 1);
+      retreat_constraints.addUnit('defender', left);
+      testLeft(area, 1);
+
+      testLeft(area, 1);
+      retreat_constraints.addUnit('attacker', left);
+      testLeft(area, 0);
+    });
+
+  });
+
+  describe("#getArrayDirection()", function() {
+
+    it('should return the correct relative direction when given an adjacent location', function() {
+      var location = { x: 24, y: 20 };
+      var retreat_constraints = new RetreatConstraints(location);
+
+      var tile = { x: 24, y: 19 };
+      var direction = retreat_constraints.getArrayDirection(tile);
+      assert.equal(direction.x, 1);
+      assert.equal(direction.y, 0);
+
+      var tile = { x: 24, y: 21 };
+      var direction = retreat_constraints.getArrayDirection(tile);
+      assert.equal(direction.x, 1);
+      assert.equal(direction.y, 2);
+
+      var tile = { x: 23, y: 20 };
+      var direction = retreat_constraints.getArrayDirection(tile);
+      assert.equal(direction.x, 0);
+      assert.equal(direction.y, 1);
+
+      var tile = { x: 25, y: 20 };
+      var direction = retreat_constraints.getArrayDirection(tile);
+      assert.equal(direction.x, 2);
+      assert.equal(direction.y, 1);
+    });
+
+    it('should throw an BadLocation error when given a location that is too far away', function() {
+      var location = { x: 15, y: 7 };
+      var retreat_constraints = new RetreatConstraints(location);
+      //var direction_ftn = retreat_constraints.getArrayDirection;
+
+      var tile = { x: 14, y: 6 };
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = { x: 14, y: 8 };
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = { x: 16, y: 6 };
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = { x: 16, y: 8 };
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = { x: 12, y: 65 };
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+    });
+
+    it('should throw an BadLocation error when given a location that is too far away', function() {
+      var location = { x: 15, y: 7 };
+      var retreat_constraints = new RetreatConstraints(location);
+
+      var tile = undefined;
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = 0;
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = undefined;
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = false;
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = { x: false, y: 7 };
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+
+      var tile = { x: 8, y: undefined };
+      assert.throws(function() {
+        retreat_constraints.getArrayDirection(tile), "BadDirection"
+      });
+    });
+  });
+
+  describe('#applyToArray()', function() {
+
+    it('should update parameter array in the correct place', function() {
+      var location = { x: 15, y: 7 };
+      var retreat_constraints = new RetreatConstraints(location);
+      var direction = { x: 15, y: 6 };
+      retreat_constraints.setSide('attacker', direction);
+      var array = getArrayOfConstant();
+
+      retreat_constraints.applyToArray(array);
+
+      assert.equal(array[15][6], 1);
+      assert.equal(array[15][8], 0);
+      assert.equal(array[14][7], 0);
+      assert.equal(array[16][7], 0);
+
+      // reset to 1's for easy test of rest of array
+      array[15][8] = 1;
+      array[14][7] = 1;
+      array[16][7] = 1;
+
+      // ensure rest of array is 1's
+      for (var x = 0; x < 20; x++) {
+        for (var y = 0; y < 20; y++) {
+          assert.equal(array[x][y], 1);
+        }
+      }
+
+    });
+
+    it('should update parameter array and not error for edges', function() {
+      var location = { x: 0, y: 0 };
+      var retreat_constraints = new RetreatConstraints(location);
+      var direction = { x: 1, y: 0 }; // right
+      retreat_constraints.setSide('attacker', direction);
+      var array = getArrayOfConstant();
+
+      retreat_constraints.applyToArray(array);
+
+      assert.equal(array[1][0], 1);
+      assert.equal(array[0][1], 0);
+
+      // reset to 1's for easy test of rest of array
+      array[1][0] = 1;
+      array[0][1] = 1;
+
+      // ensure rest of array is 1's
+      for (var x = 0; x < 20; x++) {
+        for (var y = 0; y < 20; y++) {
+          assert.equal(array[x][y], 1);
+        }
+      }
+
+    });
+
+    it('should not update parameter when values are 0 (impassable)', function() {
+      var location = { x: 15, y: 7 };
+      var retreat_constraints = new RetreatConstraints(location);
+      var direction = { x: 15, y: 6 };
+      retreat_constraints.setSide('attacker', direction);
+      var array = getArrayOfConstant(0);
+
+      retreat_constraints.applyToArray(array);
+
+      // ensure rest of array is 0's
+      for (var x = 0; x < 20; x++) {
+        for (var y = 0; y < 20; y++) {
+          assert.equal(array[x][y], 0);
+        }
+      }
+
+    });
+
+    it('should update property of parameter array', function() {
+      var location = { x: 15, y: 7 };
+      var retreat_constraints = new RetreatConstraints(location);
+      var direction = { x: 15, y: 6 };
+      retreat_constraints.setSide('attacker', direction);
+      var array = getArrayOfConstant();
+
+      var array = [];
+      for (var x = 0; x < 20; x++) {
+        array[x] = [];
+        for (var y = 0; y < 20; y++) {
+          array[x][y] = { weight: 1 };
+        }
+      }
+
+      retreat_constraints.applyToArray(array, 'weight');
+
+      assert.equal(array[15][6].weight, 1);
+      assert.equal(array[15][8].weight, 0);
+      assert.equal(array[14][7].weight, 0);
+      assert.equal(array[16][7].weight, 0);
+
+      // reset to 1's for easy test of rest of array
+      array[15][8].weight = 1;
+      array[14][7].weight = 1;
+      array[16][7].weight = 1;
+
+      // ensure rest of array is 1's
+      for (var x = 0; x < 20; x++) {
+        for (var y = 0; y < 20; y++) {
+          assert.equal(array[x][y].weight, 1);
+        }
+      }
+
+    });
+  });
+
+
+  function testAreaPosition(area, x, y, value) {
+    assert.equal(area[x][y], value); // top
+  }
+
+  function testTop(area, value) {
+    testAreaPosition(area, 1, 0, value);
+  }
+
+  function testRight(area, value) {
+    testAreaPosition(area, 2, 1, value);
+  }
+
+  function testBottom(area, value) {
+    testAreaPosition(area, 1, 2, value);
+  }
+
+  function testLeft(area, value) {
+    testAreaPosition(area, 0, 1, value);
+  }
+
+  function testAreaCornersAndCentre(area) {
+    assert.equal(area[0][0], 1);
+    assert.equal(area[0][2], 1);
+    assert.equal(area[1][1], 1);
+    assert.equal(area[2][0], 1);
+    assert.equal(area[2][2], 1);
+  }
+
+  function getArrayOfConstant(value) {
+    if (value === undefined) value = 1;
+    var array = [];
+    for (var x = 0; x < 20; x++) {
+      array[x] = [];
+      for (var y = 0; y < 20; y++) {
+        array[x][y] = value;
+      }
+    }
+    return array;
+  }
+
+});
