51161372b6ae02a6572a07e80777b70f0d725fcd
Daniel Moniz <daniel.moniz@gmail.com>
2016-02-03 14:41:04 -0500

    Ruin hex builder but make square-based terrain builder.

--- public/src/terrain_grid.js
+++ public/src/terrain_grid.js
@@ -5,6 +5,8 @@ function getURLParameter(name) {
 
 $(document).ready(function() {
 
+var gridType = getURLParameter('gridType');
+
 var width = getURLParameter('width');
 var height = getURLParameter('height');
 var location = getURLParameter('location');
@@ -17,14 +19,6 @@ var map_grid = {
 var options = {
   location: locations[location] || locations.black_forest,
   map_grid: map_grid,
-
-      board_title: {
-        height: 24,
-      },
-
-      board_tool_bar: {
-        height: 32,
-      },
 
       player_colour: { 0: "Blue", 1: "White" },
       num_sections: 1,
@@ -44,6 +38,25 @@ var terrain_map = terrain_map[0].map(function(col, i) {
 });
 console.log(terrain_map);
 
+function make_square(class_name, content, height) {
+  var square_div = jQuery('<div/>', {
+    class: class_name + ' square',
+  });
+
+  //var contents = "<span>{0}</span><div></div><div></div>".format(content);
+  //square_div.append(contents);
+
+  return square_div;
+}
+
+function add_square_row() {
+  var square_row_div = jQuery('<div/>', {
+    class: 'squarerow',
+  });
+  $('.grid').append(square_row_div);
+  return square_row_div;
+}
+
 function make_hex(class_name, content) {
   var hex_div = jQuery('<div/>', {
     class: class_name,
@@ -58,17 +71,29 @@ function add_hex_row() {
   var hex_row_div = jQuery('<div/>', {
     class: 'hexrow',
   });
-  $('.hexes').append(hex_row_div);
+  $('.grid').append(hex_row_div);
   return hex_row_div;
 }
 
-for (var i=0; i<terrain_map.length; i++) {
-  var row = add_hex_row();
-  //console.log(i);
-  for (var j=0; j < terrain_map[i].length; j++) {
-    var text = '{0}, {1}'.format(i, j);
-    var hex = make_hex(terrain_map[i][j].type, '');
-    row.append(hex);
+if (gridType == 'hex') {
+
+  for (var i=0; i<terrain_map.length; i++) {
+    var row = add_hex_row();
+    for (var j=0; j < terrain_map[i].length; j++) {
+      var text = '{0}, {1}'.format(i, j);
+      var hex = make_hex(terrain_map[i][j].type, '');
+      row.append(hex);
+    }
+  }
+} else { // default to square grid
+
+  for (var i=0; i<terrain_map.length; i++) {
+    var row = add_square_row();
+    for (var j=0; j < terrain_map[i].length; j++) {
+      var text = '{0}, {1}'.format(i, j);
+      var square = make_square(terrain_map[i][j].type, '');
+      row.append(square);
+    }
   }
 }
 
